0c846fbff5cb53dd2282c0a131a2f860e5707f95
Idong Essien <idong.essien@comcast.net>
2020-03-27 13:35:47 -0400

    MVP functionality met

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+export const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorOption, setColorOption] = useState([colors]);
 
+  console.log(colorOption)
+  
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
@@ -18,14 +21,27 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+
+    axiosWithAuth().put(`colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log(res)
+      setEditing(false)
+      updateColors(colors.map((item) => {
+        return item.id === colorToEdit.id ? colorToEdit : item
+      }))
+    })
+    .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+  
+      axiosWithAuth().delete(`/colors/${color.id}`)
+        .then(res => {
+          console.log(res)
+          setColorOption(colors.filter(item => item.id !== color.id))
+        })
   };
+
 
   return (
     <div className="colors-wrap">
@@ -80,10 +96,8 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
+       <div className="spacer"></div>
       {/* stretch - build another form here to add a color */}
     </div>
   );
-};
-
-export default ColorList;
+}
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+export const Login = (props) => {
+  // handle submit function and set state w/ useState ... confirm protects
+  const [form, setForm] = useState ({
+      username:'',
+      password:'',
+  })
+  console.log(form);
+
   // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+  const handleSubmit = (event) => {
+      event.preventDefault()
+      axiosWithAuth().post('/login', form)
+      .then(response => {
+          console.log(response)
+          localStorage.setItem('token', response.data.payload)
+          // when you have handled the token, navigate to the BubblePage route
+          props.history.push('/protected')
+      })
+      .catch(err => console.table(err))
+  }
 
-export default Login;
+  return(
+    <>
+    <h1>Welcome to the Bubble App!</h1>
+      <form onSubmit={handleSubmit}>
+      <input
+          name='username'
+          type='text'
+          placeholder='User name'
+          value={form.username}
+          onChange={(event) => {
+              setForm({
+                  ...form,
+                  [event.target.name]:event.target.value
+              })
+          }}
+          />
+      <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          value={form.password}
+          onChange={(event) => {
+              setForm({
+                  ...form,
+                  [event.target.name]:event.target.value
+              })
+          }}
+          />
+          <button type='submit'>Submit</button>
+      </form>
+      </>
+  )
+}
